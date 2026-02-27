c2e6db231a593bf77bd2b2d722a46e76a9d37018
devbones <87474953+thedevbones@users.noreply.github.com>
2026-02-27 17:35:19 -0600

    Add WASD controls, DoorCutout, and physics tweaks

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,7 +14,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("walk_left", "walk_right", "walk_forward", "walk_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
