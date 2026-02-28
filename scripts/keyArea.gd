caf6dcc03eea52b1ea6bf681d820be49229bd986
devbones <87474953+thedevbones@users.noreply.github.com>
2026-02-27 21:49:43 -0600

    Add KeyArea to unlock door with key

---
+++ scripts/keyArea.gd
@@ -0,0 +1,20 @@
+extends Area3D
+
+@export var door : Node3D
+@onready var physics_door = door.get_node("PhysicsDoor") as RigidBody3D
+@onready var light = $AreaLight
+
+func _ready() -> void:
+	light.light_color = Color.WHITE
+
+func _on_body_entered(body: Node3D) -> void:
+	print("Body entered key area: ", body.name)
+	if not body.is_in_group("key"):
+		return
+	light.light_color = Color.GREEN
+	physics_door.unlock()
+
+func _on_body_exited(body: Node3D) -> void:
+	if not body.is_in_group("key"):
+		return
+	light.light_color = Color.WHITE
